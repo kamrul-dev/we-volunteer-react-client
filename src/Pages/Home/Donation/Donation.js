34b867b6f7a782ee105b38a0db77dc85e84576fa
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-26 21:43:45 +0600

    Project Folder structured done

---
+++ src/Pages/Home/Donation/Donation.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Donation = () => {
+    return (
+        <div>
+            <h2>Donation</h2>
+        </div>
+    );
+};
+
+export default Donation;
---
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>Navigation bar</h2>
+        </div>
+    );
+};
+
+export default NavBar;
