f169fb6d8516ce9acb9f18cc4735f769ad83abcd
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-27 02:48:14 +0600

    banner content center

--- src/Pages/Home/banner/banner.js
+++ src/Pages/Home/banner/banner.js
@@ -6,9 +6,11 @@ const Banner = () => {
     return (
         <div className='hero-banner'>
             <NavBar></NavBar>
-            <div className='text-center'>
+            <div className='d-flex flex-column align-items-center'>
                 <h2 className=''>WE GROW PEOPLE FOR HELPING</h2>
-                <input className='w-50' type="text" name="" id="" placeholder='search' />
+                <div>
+                    <input className='' type="text" name="" id="" placeholder='search' />
+                </div>
             </div>
         </div>
     );
