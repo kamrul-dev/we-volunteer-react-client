50ff6f59ebe46f15f5b5974f510c8a69d84051b4
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-27 13:58:07 +0600

    navigation component adde in login and registration page

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -4,54 +4,58 @@ import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
 import { AiFillTwitterCircle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import NavBar from '../../Shared/NavBar/NavBar';
 
 const login = () => {
     return (
-        <div className='login-body'>
-            <div className="container-full">
-                <div className="item-container">
-                    <h2 className="log-in">Log in</h2>
-                </div>
-                <div className="item-container">
-                    <button>
-                        <FcGoogle />
-                    </button>
-                    <button>
-                        <BsFacebook />
-                    </button>
-                    <button>
-                        <AiFillTwitterCircle />
-                    </button>
-                </div>
-                <div className="item-container">
-                    <p>or login using email</p>
-                </div>
-                <form>
-                    <div className="form-input">
-                        <input type="email" name='email' placeholder='Email' required/>
+        <div>
+            <NavBar></NavBar>
+            <div className='login-body'>
+                <div className="container-full">
+                    <div className="item-container">
+                        <h2 className="log-in">Log in</h2>
                     </div>
-                    <div className="form-input">
-                        <input type="password" name='password' placeholder='Password' required/>
+                    <div className="item-container">
+                        <button>
+                            <FcGoogle />
+                        </button>
+                        <button>
+                            <BsFacebook />
+                        </button>
+                        <button>
+                            <AiFillTwitterCircle />
+                        </button>
                     </div>
+                    <div className="item-container">
+                        <p>or login using email</p>
+                    </div>
+                    <form>
+                        <div className="form-input">
+                            <input type="email" name='email' placeholder='Email' required />
+                        </div>
+                        <div className="form-input">
+                            <input type="password" name='password' placeholder='Password' required />
+                        </div>
+                        <div className="display-space-between">
+                            <div>
+                                <input type="checkbox" />
+                                <label htmlFor="password" className="chekbox-label">Remember Me</label>
+                            </div>
+                            <div>
+                                <Link className='text-decoration-none' to="/">Forget password</Link>
+                            </div>
+                        </div>
+                        <div>
+                            <button type="submit">Log in</button>
+                        </div>
+                    </form>
                     <div className="display-space-between">
-                        <div>
-                            <input type="checkbox"/>
-                            <label htmlFor="password" className="chekbox-label">Remember Me</label>
-                        </div>
-                        <div>
-                            <Link className='text-decoration-none' to="/">Forget password</Link>
-                        </div>
+                        <Link className='text-decoration-none' to="/">privacy policy</Link>
+                        <Link className='text-decoration-none' to="/">Terms & condition</Link>
                     </div>
                     <div>
-                        <button type="submit">Log in</button>
+                        <Link className='text-decoration-none' to="/register">Create an Account</Link>
                     </div>
-                </form>
-                <div className="display-space-between">
-                    <Link className='text-decoration-none' to="/">privacy policy</Link>
-                    <Link className='text-decoration-none' to="/">Terms & condition</Link>
-                </div>
-                <div>
-                    <Link className='text-decoration-none' to="/register">Create an Account</Link>
                 </div>
             </div>
         </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,48 +3,52 @@ import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
 import { AiFillTwitterCircle } from 'react-icons/ai';
+import NavBar from '../../Shared/NavBar/NavBar';
 
 const Register = () => {
     return (
-        <div className='login-body'>
-            <div className="container-full">
-                <div className="item-container">
-                    <h2 className="log-in">Registration</h2>
-                </div>
-                <div className="item-container">
-                    <button>
-                        <FcGoogle />
-                    </button>
-                    <button>
-                        <BsFacebook />
-                    </button>
-                    <button>
-                        <AiFillTwitterCircle />
-                    </button>
-                </div>
-                <div className="item-container">
-                    <p>or login using email</p>
-                </div>
-                <form>
-                    <div className="form-input">
-                        <input type="text" name='name' placeholder='Name' />
+        <div>
+            <NavBar></NavBar>
+            <div className='login-body'>
+                <div className="container-full">
+                    <div className="item-container">
+                        <h2 className="log-in">Registration</h2>
                     </div>
-                    <div className="form-input">
-                        <input type="email" name='email' placeholder='Email' required/>
+                    <div className="item-container">
+                        <button>
+                            <FcGoogle />
+                        </button>
+                        <button>
+                            <BsFacebook />
+                        </button>
+                        <button>
+                            <AiFillTwitterCircle />
+                        </button>
                     </div>
-                    <div className="form-input">
-                        <input type="password" name='password' placeholder='Password' required />
+                    <div className="item-container">
+                        <p>or login using email</p>
+                    </div>
+                    <form>
+                        <div className="form-input">
+                            <input type="text" name='name' placeholder='Name' />
+                        </div>
+                        <div className="form-input">
+                            <input type="email" name='email' placeholder='Email' required />
+                        </div>
+                        <div className="form-input">
+                            <input type="password" name='password' placeholder='Password' required />
+                        </div>
+                        <div>
+                            <button type="submit">Register</button>
+                        </div>
+                    </form>
+                    <div className="display-space-between">
+                        <Link className='text-decoration-none' to="/">privacy policy</Link>
+                        <Link className='text-decoration-none' to="/">Terms & condition</Link>
                     </div>
                     <div>
-                        <button type="submit">Register</button>
+                        <span>Already Have an Account?</span> <Link className='text-decoration-none' to="/login">Login</Link>
                     </div>
-                </form>
-                <div className="display-space-between">
-                    <Link className='text-decoration-none' to="/">privacy policy</Link>
-                    <Link className='text-decoration-none' to="/">Terms & condition</Link>
-                </div>
-                <div>
-                    <span>Already Have an Account?</span> <Link className='text-decoration-none' to="/login">Login</Link>
                 </div>
             </div>
         </div>
