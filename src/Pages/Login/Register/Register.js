08af3132c890bc7a348c23af353a19238e998b75
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-28 15:03:47 +0600

    toastify css link added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
